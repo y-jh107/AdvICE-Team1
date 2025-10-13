13de16595802afad1aa7b30ba1e6bbb9c83359b6
y-jh107 <jin99545@naver.com>
2025-10-13 23:16:04 +0900

    Main Controller 추가 및 임시 SecurityConfig 생성

---
+++ backend/src/main/java/com/advice/team1/backend/common/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.advice.team1.backend.common.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+// 메인 페이지 접근을 위해 임시로 작성된 파일, 필요시 수정
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                        .requestMatchers("/", "/index.html", "/favicon.ico", "/assets/**", "/static/**").permitAll()
+                        .requestMatchers("/api/**").permitAll()
+                        .anyRequest().permitAll()
+                );
+        return http.build();
+    }
+}
