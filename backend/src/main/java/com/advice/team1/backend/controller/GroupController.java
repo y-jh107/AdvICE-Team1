ea5f8ee0ba2183e85dc47093969d1777d643e04f
YoungVin Byeon <regan2003@naver.com>
2025-11-06 00:16:18 +0900

    fix(group) : GroupController 수정

--- backend/src/main/java/com/advice/team1/backend/controller/GroupController.java
+++ backend/src/main/java/com/advice/team1/backend/controller/GroupController.java
@@ -5,7 +5,6 @@ import com.advice.team1.backend.domain.dto.GroupRequestDto;
 import com.advice.team1.backend.domain.entity.Group;
 import com.advice.team1.backend.service.GroupService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,20 +15,17 @@ public class GroupController {
     private final GroupService groupService;
 
     @PostMapping
-    public ResponseEntity<ApiResponse<Object>> create(@RequestBody GroupRequestDto req) {
+    public ApiResponse<Object> create(@RequestBody GroupRequestDto req) {
         Group g = groupService.create(req);
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .body(new ApiResponse<>("SU", "모임 생성 성공.", "g_" + g.getId()));
+        return new ApiResponse<>("SU", "모임 생성 성공.", "g_" + g.getId());
     }
 
     @PatchMapping("/{groupId}")
-    public ResponseEntity<ApiResponse<Object>> update(
+    public ApiResponse<Object> update(
             @PathVariable Long groupId,
             @RequestBody GroupRequestDto req
     ) {
         Group g = groupService.update(groupId, req);
-        return ResponseEntity.ok(
-                new ApiResponse<>("SU", "모임 수정 성공.", "g_" + g.getId())
-        );
+        return new ApiResponse<>("SU", "모임 수정 성공.", "g_" + g.getId());
     }
 }
